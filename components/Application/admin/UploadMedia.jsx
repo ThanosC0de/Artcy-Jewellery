2f74d72f55025eaa9bb97e0d09a3eb0dcdcc7c75
ThanosC0de <163315295+ThanosC0de@users.noreply.github.com>
2025-07-17 21:42:31 +0530

    test

--- components/Application/admin/UploadMedia.jsx
+++ components/Application/admin/UploadMedia.jsx
@@ -1,17 +1,39 @@
+'use client'
 import React from "react";
 import { CldUploadWidget } from "next-cloudinary";
 import { Button } from "@/components/ui/button";
 import { FiPlus } from "react-icons/fi";
 import { showToast } from "@/lib/showToast";
+import axios from "axios";
 
 const UploadMedia = ({ isMultiple }) => {
   const handleOnError = (error) => {
     showToast("error", error.statusText);
   };
-  const handleOnQueuesEnd = (results) => {
-    console.log(results);
+  const handleOnQueuesEnd = async (results) => {
+    const files = results.info.files
+    const uploadedFiles = files.filter(file=>file.uploadInfo).map(file=>({
+      asset_id: file.uploadInfo.asset_id,
+      public_id: file.uploadInfo.public_id,
+      secure_url: file.uploadInfo.secure_url,
+      path: file.uploadInfo.path,
+      thumbnail_url: file.uploadInfo.thumbnail_url,
+    }))
+
+    if(uploadedFiles.length > 0 ) {
+       try {
+        const {data: mediaUploadResponse} = await axios.post('/api/media/create', uploadedFiles) 
+        if(!mediaUploadResponse.success){
+          throw new Error(mediaUploadResponse.message)
+        }
+        showToast('success'.mediaUploadResponse.message)
+
+       } catch (error) {
+        showToast('error',error.message)
+       }
+    }
   };
-  return (
+  return (  
     <CldUploadWidget
       signatureEndpoint="/api/cloudinary-signature"
       uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
